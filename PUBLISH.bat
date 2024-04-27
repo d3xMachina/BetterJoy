56f8fec88ae14e2667cca30f4fb948c62b4d1058
d3xMachina <16732772+d3xMachina@users.noreply.github.com>
2024-04-27 05:53:33 +0200

    disable microsoft telemetry on publish command

--- PUBLISH.bat
+++ PUBLISH.bat
@@ -1,4 +1,5 @@
 @echo off
+set "DOTNET_CLI_TELEMETRY_OPTOUT=1"
 set "options=--nologo --configuration Release -p:PublishSingleFile=true -p:DebugType=None -p:SelfContained=false -p:TieredPGO=true"
 set "runtime=win-x64"
 set "framework=net8.0-windows"
