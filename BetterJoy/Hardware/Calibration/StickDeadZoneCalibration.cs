17893baeda18fb42a13e3fb7e4d12c5b99aa7984
ConnorS-P <cstutsmanplunkett@mines.edu>
2025-06-12 20:41:18 -0600

    objectify-deadzone Cleanup

--- BetterJoy/Hardware/Calibration/StickDeadZoneCalibration.cs
+++ BetterJoy/Hardware/Calibration/StickDeadZoneCalibration.cs
@@ -6,39 +6,39 @@ namespace BetterJoy.Hardware.Calibration;
 
 public class StickDeadZoneCalibration
 {
-    private float value;
+    private float _value;
 
     public StickDeadZoneCalibration()
     {
-        value = 0;
+        _value = 0;
     }
-        
+
     public StickDeadZoneCalibration(float value)
     {
-        this.value = value;
+        this._value = value;
     }
-        
+
     public StickDeadZoneCalibration(StickRangeCalibration stickRangeCalibration, Span<byte> raw)
     {
         if (raw.Length != 2)
         {
             throw new ArgumentException($"{nameof(StickDeadZoneCalibration)} expects 2 bytes, got {raw.Length}.");
         }
-        
-        value = CalculateDeadZone(stickRangeCalibration, BitWrangler.Lower3NibblesLittleEndian(raw[0], raw[1]));
+
+        _value = CalculateDeadZone(stickRangeCalibration, BitWrangler.Lower3NibblesLittleEndian(raw[0], raw[1]));
     }
 
     public static StickDeadZoneCalibration FromConfigRight(ControllerConfig config)
     {
         return new StickDeadZoneCalibration(config.StickRightDeadzone);
     }
-    
+
     public static StickDeadZoneCalibration FromConfigLeft(ControllerConfig config)
     {
         return new StickDeadZoneCalibration(config.StickLeftDeadzone);
     }
 
-    public static implicit operator float(StickDeadZoneCalibration deadZone) => deadZone.value;
+    public static implicit operator float(StickDeadZoneCalibration deadZone) => deadZone._value;
 
     private static float CalculateDeadZone(StickRangeCalibration stickRangeCalibration, ushort deadZone)
     {
